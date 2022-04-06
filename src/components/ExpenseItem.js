0454a1cdce2aa56f2a4cf850bd95b27b5d379495
Ömer Aydemir <osilix1@gmail.com>
2022-04-06 18:18:37 +0300

    lesson 2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,16 @@
-
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
-    return (
-        <div className="expense-item">
-            <div>
-                March 28th 2022
-            </div>
-            <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">
-                    $294.67
-                </div>
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
